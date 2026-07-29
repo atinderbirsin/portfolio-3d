7ea8d323d6808642be0d81b2495160f8ea383f32
Atinderbir Singh <atinderbirsingh@192.168.1.2>
2026-07-29 22:16:07 +0530

    deployed

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -31,7 +31,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           </div>
           {/* Mobile photo - shows only on mobile when 3D character is hidden */}
           <div className="mobile-photo">
-            <img src="/images/mypicnbg.png" alt="Atinderbir Singh" />
+            {/* <img src="/images/mypicnbg.png" alt="Atinderbir Singh" />git  */}
           </div>
         </div>
         {children}
