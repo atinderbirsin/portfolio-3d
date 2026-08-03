6285137140beb2265c1cd22d3313708f28569d8f
Atinderbir Singh <atinderbirsingh@Atinderbirs-MacBook-Neo.local>
2026-08-04 00:19:04 +0530

    added github action

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-3d/",
+  server: {
+    open: true,
+    port: 3001,
+  },
   build: {
     rollupOptions: {
       output: {
